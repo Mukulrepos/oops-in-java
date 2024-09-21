3c46bb88d94d788c90380419dc2268c309f3a6ed
Mukulrepos <mv282301@gmail.com>
2024-09-21 15:13:43 +0530

    delautl consttructor is used to defalut type

---
+++ defaults.java
@@ -0,0 +1,16 @@
+class car {
+    car(){
+        System.out.println("hello i am car");
+        
+    }
+}
+
+
+public class defaults {
+
+    public static void main(String[] args) {
+        car start = new car();
+        
+    }
+    
+}
