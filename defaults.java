dd69e43ac236143728490d9315e4a500208b1e05
Mukulrepos <mv282301@gmail.com>
2024-09-21 15:14:28 +0530

    deafult constructor

--- defaults.java
+++ defaults.java
@@ -1,16 +1,15 @@
 class car {
-    car(){
+    car() {
         System.out.println("hello i am car");
-        
+
     }
 }
-
 
 public class defaults {
 
     public static void main(String[] args) {
         car start = new car();
-        
+
     }
-    
+
 }
